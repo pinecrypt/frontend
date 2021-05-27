4254a9c567cc5a30064a835c5bbb726575df9e6a
Lauri Võsandi <lauri@pinecrypt.com>
2021-05-27 13:58:07 +0300

    Update certidude client Git repo URL

--- templates/snippets/certidude-client.sh
+++ templates/snippets/certidude-client.sh
@@ -1,2 +1,2 @@
-pip3 install --upgrade git+http://git.k-space.ee/pinecrypt/pinecrypt-client.git
+pip3 install --upgrade git+http://git.k-space.ee/pinecrypt/certidude
 certidude provision {{ authority.namespace }}
